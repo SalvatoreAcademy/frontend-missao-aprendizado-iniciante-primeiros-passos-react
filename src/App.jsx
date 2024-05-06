4e1c15e18405a20823a900ad0ab7989592699aad
Paulo Salvatore <salvatoregames@gmail.com>
2024-05-06 19:55:10 +0000

    Criação do HTML e CSS para o Card

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
   return (
     <>
-      App!!
+      <div className="card">
+        <h1>Java</h1>
+        <img src="https://salvatore.academy/devmon/1_java.png" width="200" />
+      </div>
     </>
   )
 }
